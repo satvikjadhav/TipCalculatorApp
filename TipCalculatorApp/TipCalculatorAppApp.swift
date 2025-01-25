2210b6cb2d86fc1e816a38186aae71679cbe31c0
satvikjadhav <satvikj5@gmail.com>
2025-01-25 14:52:11 -0600

    Initial Commit

---
+++ TipCalculatorApp/TipCalculatorAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TipCalculatorAppApp.swift
+//  TipCalculatorApp
+//
+//  Created by Satvik  Jadhav on 1/25/25.
+//
+
+import SwiftUI
+
+@main
+struct TipCalculatorAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
