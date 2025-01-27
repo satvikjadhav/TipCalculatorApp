f14ab0fcb5fceb86e6e86e5239a675da024de0d7
satvikjadhav <satvikj5@gmail.com>
2025-01-27 12:29:07 -0600

    adding variables

--- TipCalculatorApp/ContentView.swift
+++ TipCalculatorApp/ContentView.swift
@@ -8,6 +8,11 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var billAmount: Double = 0.0
+    @State private var tipPercentage: Double = 15.0
+    @State private var numberOfPeople: Int = 1
+    @State private var showCurrency: String = "USD"
+    @State private var showResult: Bool = false
     
     var body: some View {
         Text("Hello, World!")
