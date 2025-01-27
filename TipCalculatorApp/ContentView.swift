113c8d59024354368d1f49755433171a38aee995
satvikjadhav <satvikj5@gmail.com>
2025-01-27 12:31:16 -0600

    adding calculated variables and curriencies

--- TipCalculatorApp/ContentView.swift
+++ TipCalculatorApp/ContentView.swift
@@ -10,9 +10,23 @@ import SwiftUI
 struct ContentView: View {
     @State private var billAmount: Double = 0.0
     @State private var tipPercentage: Double = 15.0
-    @State private var numberOfPeople: Int = 1
+    @State private var numberOfPeople: Double = 1.0
     @State private var showCurrency: String = "USD"
     @State private var showResult: Bool = false
+    
+    private var tipAmount: Double {
+        billAmount * (tipPercentage / 100)
+    }
+    
+    private var totalAmount: Double {
+        billAmount + tipAmount
+    }
+    
+    private var amountPerPerson: Double {
+        totalAmount / numberOfPeople
+    }
+    
+    private let curriencies : [String] = ["USD", "EUR", "GBP", "JPY"]
     
     var body: some View {
         Text("Hello, World!")
